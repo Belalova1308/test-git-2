7393d3d6cd27889c31fefee08615a38071428796
Olya Belalova <oybelalova@gmail.com>
2022-03-06 11:28:00 +0300

    add changes master

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+console.log('Hello World!');
+const userage=35;
+if (userage>18){
+    console.log('Вы совр=ершеннолетний')
+} else{
+    console.log('Вы не совершеннолетний')
+}
